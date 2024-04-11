f48ecde8bc4fe2d7c4f3a03339f9a9d2203b1b75
Steve <steve.gitprofile@outlook.com>
2024-04-11 19:31:22 +0200

    added gelocation api

--- rsc/js/script.js
+++ rsc/js/script.js
@@ -1,3 +1,15 @@
 'use strict';
 import { domEls } from './domElements.js';
 import { months } from './months.js';
+
+// GEOLOCATION API
+// - takes two functions - one for success one for error
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude, longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    () => alert('Could not get your position')
+  );
+}
